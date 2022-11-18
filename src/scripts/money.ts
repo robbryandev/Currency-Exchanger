aa5932d690e096f70e6eeffb263102c7b28c2ff1
robbryandev <robbryandev@gmail.com>
2022-11-18 09:11:15 -0800

    Add conversion methods

--- src/scripts/money.ts
+++ src/scripts/money.ts
@@ -1,4 +1,4 @@
-enum Currency {
+export enum Currency {
   USD,
   CAD,
   MXN,
@@ -10,8 +10,40 @@ enum Currency {
 export default class Money {
   val: number;
   to: Currency;
+  curList: string[] = this.getCurrencies();
   constructor(val: number, to: Currency) {
     this.val = val;
     this.to = to;
+  }
+
+  async getValue(): Promise<string | boolean> {
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+    return new Promise((resolve) => {
+      fetch(url)
+        .then((res) => {
+          if (res.ok) {
+            res.json()
+              .then((jres) => {
+                const data = jres.conversion_rates[this.curList[this.to]];
+                resolve(this.formatValue(data));
+              });
+          } else {
+            resolve(false);
+          }
+        });
+    });
+  }
+
+  formatValue(newVal: string) {
+    const num = parseFloat(newVal) * this.val;
+    return num.toFixed(2);
+  }
+
+  getCurrencies(): string[] {
+    const res: string[] = [];
+    for (let c = 0; c < 6; c++) {
+      res.push(Currency[c]);
+    }
+    return res;
   }
 }
